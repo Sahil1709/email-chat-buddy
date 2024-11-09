0e2c6ccb20a178e5785aa7bedc638727a11074b3
sahil <sahilofficial17@gmail.com>
2024-11-09 12:32:24 -0800

    added Request import and updated fastapi dependency to include standard extras

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from google.auth.transport.requests import Request
 
 app = FastAPI(title="Email RAG API")
 
